7a6768d82e151217394e9ceecd1b44c3b07273b9
Floris Jansen <florismartijnjansen@gmail.com>
2021-12-16 23:00:39 +0100

    init photo showcase

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,32 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { request } from "../lib/datocms";
 
-export default function Home() {
+const HOMEPAGE_QUERY = `query HomePage($limit: IntType, $maxH: IntType, $maxW: IntType ) {
+  allPhotos(first: $limit) {
+    location
+    image {
+      url(imgixParams: {maxH: $maxH, maxW: $maxW})
+    }
+  }
+}`;
+
+export async function getStaticProps() {
+  const data = await request({
+    query: HOMEPAGE_QUERY,
+    variables: {
+      limit: 10,
+      maxH: 1920,
+      maxW: 1080
+    }
+  });
+  return {
+    props: { data }
+  };
+}
+
+export default function Home({ data }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,56 +36,23 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {
+          data.allPhotos.map((item, key) => {
+            return (
+              <article className={styles.photo}>
+                <img src={item.image.url} className={styles.image} />
+                <p className={styles.location}>
+                  {item.location}
+                </p>
+              </article>
+            )
+          })
+        }
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+        <a href="https://fmjansen.com" rel="noopener noreferrer">
+          Floris Jansen
         </a>
       </footer>
     </div>
