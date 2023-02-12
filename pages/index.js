2047f205927fd6a9c02df28be656e171f01478ee
Floris Jansen <florismartijnjansen@gmail.com>
2023-02-12 20:36:11 +0100

    spaces to tabs, remove obsolete file

--- pages/index.js
+++ pages/index.js
@@ -4,46 +4,46 @@ import styles from '../styles/Home.module.css'
 import { request } from '../lib/files';
 
 export async function getStaticProps() {
-  const data = await request();
-  return {
-    props: { data }
-  };
+	const data = await request();
+	return {
+		props: { data }
+	};
 }
 
 export default function Home({ data }) {
-  return (
-    <div id="photos" className={styles.container}>
-      <Head>
-        <title>Photos - Floris Jansen</title>
-        <meta name="description" content="Photos by Floris Jansen." />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div id="photos" className={styles.container}>
+			<Head>
+				<title>Photos - Floris Jansen</title>
+				<meta name="description" content="Photos by Floris Jansen." />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
 
-      <button className={styles.navigation} onClick={() => document.getElementById('photos').scrollTo({top: document.getElementById('photos').scrollTop + window.innerHeight, behavior: 'smooth'})}>↓</button>
+			<button className={styles.navigation} onClick={() => document.getElementById('photos').scrollTo({top: document.getElementById('photos').scrollTop + window.innerHeight, behavior: 'smooth'})}>↓</button>
 
-      <main className={styles.main}>
-        {
-          data.map((item, key) => {
-            return (
-              <>
-                <p className={styles.location}>
-                  {item.location}
-                </p>
-                <article className={styles.photo} key={key}>
-                  <img src={`photos/${item.filename}`} className={styles.image} loading="lazy" />
-                </article>
-              </>
-            )
-          })
-        }
-      </main>
+			<main className={styles.main}>
+				{
+					data.map((item, key) => {
+						return (
+							<>
+								<p className={styles.location}>
+									{item.location}
+								</p>
+								<article className={styles.photo} key={key}>
+									<img src={`photos/${item.filename}`} className={styles.image} loading="lazy" />
+								</article>
+							</>
+						)
+					})
+				}
+			</main>
 
-      <footer className={styles.footer}>
-        <a href="https://fmjansen.com" rel="noopener noreferrer">
-          Floris Jansen
-        </a>
-      </footer>
-    </div>
-  )
+			<footer className={styles.footer}>
+				<a href="https://fmjansen.com" rel="noopener noreferrer">
+					Floris Jansen
+				</a>
+			</footer>
+		</div>
+	)
 }
